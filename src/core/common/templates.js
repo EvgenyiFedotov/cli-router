7d49dcdf5d0a9cef8dffdea838d9786cccf6ca2a
e.fedotov <fedotov1290@gmail.com>
2019-06-24 12:28:02 +0300

    add struct functional `templates`

---
+++ src/core/common/templates.js
@@ -0,0 +1,46 @@
+/**
+ * Create template
+ *
+ * @param {string} nameModule
+ * @param {string} nameTemplate
+ * @param {Array<string>} files
+ *
+ * @returns {Promise<undefined>}
+ */
+const createTemplate = async (nameModule, nameTemplate, files) => {};
+
+/**
+ * Delete template
+ *
+ * @param {string} nameModule
+ * @param {string} nameTemplate
+ *
+ * @returns {Promise<undefined>}
+ */
+const deleteTemplate = async (nameModule, nameTemplate) => {};
+
+/**
+ * Run template
+ *
+ * @param {string} nameModule
+ * @param {string} nameTemplate
+ *
+ * @returns {Promise<undefined>}
+ */
+const runTemplate = async (nameModule, nameTemplate) => {};
+
+/**
+ * Get list templates
+ *
+ * @param {string} nameModule
+ *
+ * @returns {Array<string>} List templates in module
+ */
+const getListTemplates = (nameModule) => {};
+
+/**
+ * Create api by nameModule
+ *
+ * @param {string} nameModule
+ */
+const createApiTemplates = nameModule => ({});
